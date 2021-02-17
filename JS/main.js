e66d58066eb6ba63ac354e6e4cd78213d91108de
SoaresVictor-vso <79172860+SoaresVictor-vso@users.noreply.github.com>
2021-02-17 00:29:03 -0300

    Revert "Soma"
    This reverts commit 59b9527812d7adf5d9e61e90642f43587bdf3eec.

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,9 @@
-var val = 0;
 var a = window.document.getElementById('div2')
 a.addEventListener('click', clicar)
-var b = window.document.getElementById('num')
-b.innerText = val
-
-
-var p1, p2, r;
-
 
 
 function clicar()
 {
-    /*a.innerText = "Obrigado por clicar!"
-    a.style.backgroundColor = '#000000';*/
-    val ++
-    b.innerText = val
-}
-
-function somar()
-{
-    p1 = Number(window.document.getElementById('ent1').value)
-    p2 = Number(window.document.getElementById('ent2').value)
-    r = p1+p2
-    window.document.getElementById('result').innerHTML= `O resultado de ${p1} + ${p2} = <b>${r}</b>.`
+    a.innerText = "Obrigado por clicar!"
+    a.style.backgroundColor = '#000000';
 }

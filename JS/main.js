d0f5fd2c8ca930a4e4931b58ff8d30fdb4c4781b
SoaresVictor-vso <79172860+SoaresVictor-vso@users.noreply.github.com>
2021-02-17 00:29:18 -0300

    Revert "SiteInit"
    This reverts commit 953fbc9f446cf3c73e289cc949de35770928032b.

--- JS/main.js
+++
@@ -1,9 +0,0 @@
-var a = window.document.getElementById('div2')
-a.addEventListener('click', clicar)
-
-
-function clicar()
-{
-    a.innerText = "Obrigado por clicar!"
-    a.style.backgroundColor = '#000000';
-}
